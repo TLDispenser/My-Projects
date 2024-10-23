60a16faf9fb71dab71fb6e1ef34b302de5ccfe76
Miluigi66 <68747511+Miluigi66@users.noreply.github.com>
2024-10-23 13:36:37 -0400

    Put the proper one there total.py

--- total.py
+++ total.py
@@ -13,7 +13,8 @@ TEXTURE_SIZE = 16
 TILE_SIZE = int((SCREEN_WIDTH / 2) / MAP_SIZE)
 FOV =  math.pi / 3
 HALF_FOV = FOV / 2
-CASTED_RAYS = TEXTURE_SIZE * 6
+TILES_TO_SEE = 6
+CASTED_RAYS = TEXTURE_SIZE * TILES_TO_SEE
 STEP_ANGLE = FOV / CASTED_RAYS
 MAX_DEPTH = int(MAP_SIZE * TILE_SIZE)
 SCALE = (SCREEN_WIDTH / 2) / CASTED_RAYS
@@ -28,10 +29,8 @@ player_x = (SCREEN_WIDTH / 2) / 2
 player_y = (SCREEN_WIDTH / 2) / 2
 player_angle = math.pi
 
-
-
+#texture 1
 WALL1_TEXTURE = (
-    "0000000101111111" #just added this to make debug ezey
     "0000000000000000"
     "0111111111111110"
     "0111111111111110"
@@ -47,7 +46,7 @@ WALL1_TEXTURE = (
     "0111111111111110"
     "0111111111111110"
     "0111111111111110"
-    #"0000000000000000"
+    "0000000000000000"
     )
 #map
 MAP = (
@@ -74,6 +73,7 @@ MAP = (
     "#                    #"
     "######################"
 )
+
 #init pygame
 pygame.init()
 
@@ -81,7 +81,26 @@ pygame.init()
 win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Ray-casting')
 
+#init timer
+clock = pygame.time.Clock()
 
+def draw_map():
+    #iterate over map
+    for i in range(MAP_SIZE):
+        for j in range(MAP_SIZE):
+            #calculate square index
+            square = i * MAP_SIZE + j
+
+            #draw map
+            pygame.draw.rect(
+                win, (191, 191, 191) if MAP[square] != ' ' else (65, 65, 65),
+                (j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE - 1, TILE_SIZE - 1)
+            )
+
+    #draw player
+    pygame.draw.circle(win, (162, 0, 255), (int(player_x), int(player_y)), 12)
+
+#ray-casting algorithm
 def ray_casting():
     #left angle of FOV
     start_angle = player_angle - HALF_FOV
@@ -139,19 +158,16 @@ def ray_casting():
                 run_length = 1
     result.append(current_run_character)
     result.append(run_length)
-    print(f"Casting summerised{result}")
     for pixel_y_pozition in range(TEXTURE_SIZE):
         #counter for walls
-        #texture_counter = 0 whent to where_and_how_much
-        texture_counter_increaser = 1
+        texture_counter = 0
         number_of_symbol_prevous = 0
         temp_depth_range_counter = 0
         for where_and_how_much in range(0, len(result), 2):
-            texture_counter = result[where_and_how_much + 1] / TEXTURE_SIZE
+            texture_counter = result[where_and_how_much + 1] % TEXTURE_SIZE
+            texture_counter_increaser = result[where_and_how_much + 1] / TILES_TO_SEE #TEXTURE_SIZE
             for number_of_symbol in range(result[where_and_how_much + 1]):
                 depth = temp_depth_list[temp_depth_range_counter]
-                if pixel_y_pozition == 0:
-                    print(f"{result[where_and_how_much]}, Line: {pixel_y_pozition}, {number_of_symbol} / {result[where_and_how_much + 1]} of {result[where_and_how_much + 1]}, {number_of_symbol + number_of_symbol_prevous + 1} / {CASTED_RAYS} rays, {temp_depth_range_counter + 1} / {len(temp_depth_list)} depths = depth of {depth}, (Increasing by {texture_counter_increaser}) {texture_counter} / {TEXTURE_SIZE} int(pixel#on texture), texture counter on: cololm: {texture_counter} row: {pixel_y_pozition} ({int(texture_counter) + (pixel_y_pozition * TEXTURE_SIZE)} / {TEXTURE_SIZE * TEXTURE_SIZE}) wich is a {WALL1_TEXTURE[int(texture_counter) + (pixel_y_pozition * TEXTURE_SIZE)]}")
                 #calculate wall_height
                 wall_height = MAX_WALL_HEIGHT / (depth) #21000
 
@@ -160,7 +176,6 @@ def ray_casting():
                     wall_height = SCREEN_HEIGHT
                     
                 if result[where_and_how_much] == '#':
-                    #print(f"{texture_counter + (pixel_y_pozition * TEXTURE_SIZE)} / {len(WALL1_TEXTURE)}")
                     color1 = (int(WALL1_TEXTURE[int(texture_counter) + (pixel_y_pozition * TEXTURE_SIZE)]) * 255) / (1 + depth * depth * 0.0001)
                     color2 = (int(WALL1_TEXTURE[int(texture_counter) + (pixel_y_pozition * TEXTURE_SIZE)]) * 255) / (1 + depth * depth * 0.0001)
                     color3 = (int(WALL1_TEXTURE[int(texture_counter) + (pixel_y_pozition * TEXTURE_SIZE)]) * 255) / (1 + depth * depth * 0.0001)
@@ -171,21 +186,7 @@ def ray_casting():
                 #draw 3D projection
                 """pygame.draw.rect(what screen? , color (hex), (x, y, width, hight)"""
                 pygame.draw.rect(win, (color1, color2, color3),  (SCREEN_WIDTH  / 2 + ((number_of_symbol + number_of_symbol_prevous) * SCALE),  ((SCREEN_HEIGHT / 2) - (wall_height / 2)) + (((wall_height / TEXTURE_SIZE) * (pixel_y_pozition))) , SCALE, wall_height /  (TEXTURE_SIZE / 2)))
-                # FIX HERE TO FIX WARPING OF WALLS UDWAUDHWAUDYHUI 
-                #texture_counter = int(number_of_symbol / TEXTURE_SIZE) #* TEXTURE_SIZE
-                # ALSO FIGURE OUT HOW TO DITECT 1/16 of a  wall and so on !!!!!!
-                #(number_of_symbol + number_of_symbol_prevous)
-                """RENEMBER EVERYTHING IS COLLOR NOT THE DRAWLING
-                
-                18 / 16 = 1.125
-                
-                16 * .125 = 2
-                
-                Maybe use that  ( % because 18 % 16 = 2)
-                
-                """
-                texture_counter_increaser = 1
-                texture_counter += texture_counter_increaser  #(number_of_symbol / TEXTURE_SIZE)
+                texture_counter += round(texture_counter_increaser)  #(number_of_symbol / TEXTURE_SIZE)
                 if texture_counter  >= (TEXTURE_SIZE): 
                     texture_counter = 0
                 temp_depth_range_counter += 1
@@ -194,8 +195,71 @@ def ray_casting():
             #int(texture_counter)      
             number_of_symbol_prevous +=  result[where_and_how_much + 1]
     pygame.display.flip()
+#movement direction
+forward = True
+#game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+
+    #convert player x, y coordinates to map col, row
+    col = int(player_x / TILE_SIZE)
+    row = int(player_y / TILE_SIZE)  
+
+    #calculate map square index
+    square = row * MAP_SIZE + col
+
+    # player hits the wall (collision detection)
+    if MAP[square] != ' ': 
+        if forward:
+            player_x -= -1 * math.sin(player_angle) * 5
+            player_y -= math.cos(player_angle) * 5
+        else:
+            player_x += -1 * math.sin(player_angle) * 5
+            player_y += math.cos(player_angle) * 5
+            
     
-pygame.draw.rect(win, (100, 100, 100), (480, SCREEN_HEIGHT / 2, SCREEN_HEIGHT, SCREEN_HEIGHT))
-pygame.draw.rect(win, (200, 200, 200), (480, -SCREEN_HEIGHT / 2, SCREEN_HEIGHT, SCREEN_HEIGHT))
-ray_casting()
-input("Contenu? ")
+    #update 2D background
+    pygame.draw.rect(win, (0, 0, 0), (0, 0, SCREEN_HEIGHT, SCREEN_HEIGHT))
+
+    #update 3D background
+    pygame.draw.rect(win, (100, 100, 100), (480, SCREEN_HEIGHT / 2, SCREEN_HEIGHT, SCREEN_HEIGHT))
+    pygame.draw.rect(win, (200, 200, 200), (480, -SCREEN_HEIGHT / 2, SCREEN_HEIGHT, SCREEN_HEIGHT))
+
+    draw_map()
+    ray_casting()
+
+    #get user input
+    keys_turn = pygame.key.get_pressed()
+
+    #handle user input
+    if keys_turn[pygame.K_LEFT]:
+        #working with radians, not degrees
+        player_angle -= 0.1
+    elif keys_turn[pygame.K_RIGHT]:
+        player_angle += 0.1
+        
+    keys_move = pygame.key.get_pressed()
+    
+    if keys_move[pygame.K_UP]:
+        forward = True
+        player_x += -1 * math.sin(player_angle) * 5
+        player_y += math.cos(player_angle) * 5
+    elif keys_move[pygame.K_DOWN]:
+        forward = False
+        player_x -= -1 * math.sin(player_angle) * 5
+        player_y -= math.cos(player_angle) * 5
+
+    
+    #set FPS
+    clock.tick(30)
+
+    #set FPS
+    fps = str(int(clock.get_fps()))
+    font = pygame.font.SysFont('Arial', 30)
+    fpssurface = font.render(fps, False, (255, 255, 255))
+    win.blit(fpssurface, (int(SCREEN_WIDTH / 2), 0))
+    #update display
+    pygame.display.flip()
