4e7b013cc1c77b847940541319fb1bd9d8c27f0c
Milo Troup <68747511+Miluigi66@users.noreply.github.com>
2025-01-10 13:41:13 -0500

    Create stl_to_funnyshape.py

---
+++ small_projects/modler_thing/stl_to_funnyshape.py
@@ -0,0 +1,28 @@
+import numpy as np
+import stl
+
+def parse_stl(stl_file):
+
+    # Load the STL file
+    mesh_data = stl.mesh.Mesh.from_file(stl_file)
+
+    # Extract vertices and faces
+    vertices = mesh_data.vectors.reshape((-1, 3))
+    faces = np.arange(len(vertices)).reshape((-1, 3))
+
+    return vertices, faces
+
+vertices, faces= parse_stl("bulbasaur_starter_1gen_flowalistik.STL")
+
+data = {
+    "vertices": vertices.tolist(),
+    "pivot": (0, 0, 0),
+    "edges": faces.tolist(),
+    "faces": [
+        (face.tolist(), (255, 0, 0)) for face in faces
+    ]
+}
+
+shape_name = input("Enter the name of the shape: ")
+with open("modles.py", "a") as file:
+    file.write(f"\nTEMP_MOVE = {{'{shape_name}': {data}}}")
