f32f3b85c8c2781c73509c01de75ec1028d758bf
Milo Troup <68747511+Miluigi66@users.noreply.github.com>
2025-02-18 20:15:48 -0500

    Create madlib.py

---
+++ small_projects/madlib.py
@@ -0,0 +1,13 @@
+print(f"\
+Welcome to the Madlib Game!\n\
+Please fill in the blanks with the requested words.\n\
+Once you have filled in all the blanks, the Madlib will be generated.\n\
+")
+print(f"\
+Before you Informal {input('Input a Verb: ')} for Halloween {input('Input a Verb (ing): ')}, make a list of {input('Input a Pronoun: ')} you will need. A list will keep you from having to make several trips to the {input('Input a Noun: ')} while you are trying to get {input('Input a Noun (Plural): ')} ready for trick or treating or trying to get your {input('Input a Place: ')} ready for the Halloween party.\n\
+The price of {input('Input a Noun: ')} increases as the holiday {input('Input a Verb: ')}. Write the name of the store with the best prices for each item on {input('Input an Adjective: ')} list. This will not only make your shopping easier, but it will also save you {input('Input a Noun: ')}. Remember to check your Sunday {input('Input a Noun: ')} for coupons you can use for a discount on Halloween {input('Input a Determiner: ')}.\n\
+You can buy new decorations {input('Input a Verb (Past tense): ')} year and keep them {input('Input an Adjective: ')} in {input('Input a Noun (Plural): ')} bins. Color-code the bins for different holidays to stay {input('Input an Adjective: ')}.\n\
+Get all of your fall and Halloween {input('Input a Noun (Plural): ')} together and sort through them and throw away the ones that are {input('Input an Adjective: ')} or just look worn. Before you begin {input('Input a Verb (ing): ')}, make sure you {input('Input a Verb: ')} your home and remove all {input('Input a Noun: ')}. This will make it much {input('Input an Adjective: ')} to decorate for Halloween, Thanksgiving, and Christmas.\n\
+Look for fun {input('Input a Noun: ')} and {input('Input a Noun: ')} such as haunted houses, hayrides, and {input('Input a fun activity: ')}. Mark the dates and locations on the family calendar to avoid scheduling conflicts.\n\
+Getting the right {input('Input a Something to wear: ')} for your children and yourself can be the most time-consuming and expensive part of Halloween. If you are making costumes from scratch, you need to allow {input('Input a Pronoun: ')} plenty of time to get them {input('Input a Verb (Past tense): ')}. You don’t want to be rushing around trying to put last-minute details on the {input('Input a Noun (Plural): ')}. If you are buying costumes, you should get them {input('Input an Adverb: ')} as possible. It may be more cost-effective to {input('Input a Verb: ')} costumes instead of {input('Input a Verb (ing): ')} them.\n\
+")
